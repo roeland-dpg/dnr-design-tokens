9f919c3242c7e557b34ccc71950dd5b63f93d97a
Roeland de Betue <roeland.de.betue@persgroep.net>
2022-11-14 12:47:47 +0100

    Update config

--- config.js
+++ config.js
@@ -1,16 +1,23 @@
+/** @type {import('style-dictionary').Config} */
+
 module.exports =
     {
         source: ["data/**/*.json"],
         platforms: {
             scss: {
+                prefix: 'dnr',
                 transformGroup: "scss",
                 buildPath: "build/scss/",
                 files: [{
                     destination: "_variables.scss",
                     format: "scss/variables"
-                }]
+                }],
+                options: {
+                    outputReferences: true
+                }
             },
             "css": {
+                prefix: 'dnr',
                 "transformGroup": "css",
                 buildPath: "build/css/",
                 "files": [
@@ -18,11 +25,10 @@ module.exports =
                         "format": "css/variables",
                         "destination": "variables.css"
                     }
-                ]
+                ],
+                options: {
+                    outputReferences: true
+                }
             }
         },
     }
-
-
-
-
