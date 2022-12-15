1171f03e86f4057505e6bf400ecf4e66074baae0
Roeland de Betue <roeland.de.betue@persgroep.net>
2022-12-15 15:15:10 +0100

    Update build setup

--- config.js
+++ config.js
@@ -1,31 +1,71 @@
 /** @type {import('style-dictionary').Config} */
 
+const StyleDictionary = require('style-dictionary');
+const core = require('./data/core.json');
+const categories = Object.keys(core)
+
+const componentsFile = require('./data/components.json');
+const components = Object.keys(componentsFile)
+
+StyleDictionary.registerTransform({
+    name: "shadow/css",
+    type: "value",
+    transitive: true, // Necessary when the color is an alias reference, or the shadows themselves are aliased
+    matcher: (token) => token.type === "boxShadow",
+    transformer: (token) => {
+        // Allow both single and multi shadow tokens:
+        const shadows = Array.isArray(token.value) ? token.value : [token.value];
+
+        const transformedShadows = shadows.map((shadow) => {
+            const {x, y, blur, spread, color, type} = shadow;
+            const inset = type === "innerShadow" ? "inset " : "";
+            return `${inset}${x}px ${y}px ${blur}px ${spread}px ${color}`;
+        });
+
+        return transformedShadows.join(", ");
+    },
+});
+
+StyleDictionary.registerTransform({
+    name: "size/check-px",
+    type: "value",
+    transitive: true, // Necessary when the color is an alias reference, or the shadows themselves are aliased
+    transformer: (token) => {
+        if (!isNaN(token.value)) return `${token.value}px`
+        return token.value
+    },
+});
+
 module.exports =
     {
         source: ["data/**/*.json"],
         platforms: {
-            scss: {
+            css: {
                 prefix: 'dnr',
-                transformGroup: "scss",
-                buildPath: "build/scss/",
-                files: [{
-                    destination: "_variables.scss",
-                    format: "scss/variables"
-                }],
-                options: {
-                    outputReferences: true
-                }
-            },
-            "css": {
-                prefix: 'dnr',
-                "transformGroup": "css",
+                transforms: [
+                    "size/check-px",
+                    'attribute/cti',
+                    'name/cti/kebab',
+                    'time/seconds',
+                    'content/icon',
+                    'shadow/css',
+                    'color/css'],
                 buildPath: "build/css/",
-                "files": [
-                    {
-                        "format": "css/variables",
-                        "destination": "variables.css"
-                    }
-                ],
+                files: [...categories.map(category => ({
+                    filter: (token) => {
+                        console.log(token)
+                        return token.attributes.category === category
+                    },
+                    format: "css/variables",
+                    destination: `${category}.css`
+                })), ...components.map(component => ({
+                    filter: (token) => {
+                        return token.attributes.category === component
+                    },
+                    format: "css/variables",
+                    destination: `${component}.css`
+                }))]
             }
         },
+
     }
